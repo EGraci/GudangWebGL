a89927953d0a590b9eb6990c626c3f77a01bae3a
Graci <44756651+EGraci@users.noreply.github.com>
2021-04-14 22:30:46 +0700

    gl.clearColor(0, 0, 0, 0);
    Warna canvas disamakan dengan yang di awal

--- docs/scaling.js
+++ docs/scaling.js
@@ -244,7 +244,7 @@ gl.enable(gl.DEPTH_TEST);
 
 // gl.depthFunc(gl.LEQUAL);
 
-gl.clearColor(0.1, 0.1, 0.1, 0);
+gl.clearColor(0, 0, 0, 0);
 gl.clearDepth(1.0);
 gl.viewport(0.0, 0.0, canvas.width, canvas.height);
 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
